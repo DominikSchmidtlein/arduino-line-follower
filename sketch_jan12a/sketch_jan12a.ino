82fae4b66449e15c62df517209ad56c6a5f37f13
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2018-02-02 16:00:59 -0500

    original line follower logic

--- sketch_jan12a/sketch_jan12a.ino
+++ sketch_jan12a/sketch_jan12a.ino
@@ -8,11 +8,11 @@
 #define LEFT_IR A0
 #define RIGHT_IR A1
 
-#define P100 175
+#define P100 250
 #define P50 127
 #define P0 0
 
-#define IR_THRESHOLD 100
+#define IR_THRESHOLD 300
 
 typedef enum { FWD, RVS, STP, LFT, RHT } Dir;
 typedef enum { WHITE, BLACK } Color;
@@ -39,34 +39,35 @@ void loop() {
   left_ir = analogRead(LEFT_IR);
   right_ir = analogRead(RIGHT_IR);
 
-  Serial.print(left_ir);
-  Serial.print(", ");
-  Serial.println(right_ir);
+  Serial.println(left_ir);
+//  Serial.print(", ");
+//  Serial.println(right_ir);
 
   left_col = read_ir(left_ir);
   right_col = read_ir(right_ir);
 
   if (left_col == WHITE && right_col == WHITE) {
-    motor(LFT);
-  } else if (left_col == BLACK && right_col == WHITE) {
     motor(FWD);
+  } else if (left_col == BLACK && right_col == WHITE) {
+    motor(LFT);
   } else if (left_col == WHITE && right_col == BLACK) {
     motor(RHT);
   } else {
     motor(RHT);
   }
-  delay(50);
+  delay(40);
+  quick_stop();
+  delay(80);
 }
 
 void motor(Dir dir) {
- quick_stop();
   switch(dir) {
     case FWD:
       // set the motor speed and direction
       digitalWrite( MOTOR_L_DIR, HIGH ); // direction = forward
-      analogWrite( MOTOR_L_PWM, P100 ); // PWM speed = fast
+      analogWrite( MOTOR_L_PWM, 255-P100 ); // PWM speed = fast
       digitalWrite( MOTOR_R_DIR, HIGH ); // direction = forward
-      analogWrite( MOTOR_R_PWM, P100 ); // PWM speed = fast
+      analogWrite( MOTOR_R_PWM, 255-P100 ); // PWM speed = fast
       break;
     case RVS:
       // set the motor speed and direction
@@ -80,12 +81,12 @@ void motor(Dir dir) {
       digitalWrite( MOTOR_L_DIR, LOW ); // direction = forward
       analogWrite( MOTOR_L_PWM, P100 ); // PWM speed = fast
       digitalWrite( MOTOR_R_DIR, HIGH ); // direction = forward
-      analogWrite( MOTOR_R_PWM, P100 ); // PWM speed = fast
+      analogWrite( MOTOR_R_PWM, 255-P100 ); // PWM speed = fast
       break;
     case RHT:
       // set the motor speed and direction
       digitalWrite( MOTOR_L_DIR, HIGH ); // direction = forward
-      analogWrite( MOTOR_L_PWM, P100 ); // PWM speed = fast
+      analogWrite( MOTOR_L_PWM, 255-P100 ); // PWM speed = fast
       digitalWrite( MOTOR_R_DIR, LOW ); // direction = forward
       analogWrite( MOTOR_R_PWM, P100 ); // PWM speed = fast
       break;
